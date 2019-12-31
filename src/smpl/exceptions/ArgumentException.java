6db5a5fd66254a1d7cf1c34e60715c93d2baece6
Rowan Atkinson <rowaneatkinson@gmail.com>
2019-12-30 23:37:51 -0500

    function calls implemented

--- src/smpl/exceptions/ArgumentException.java
+++ src/smpl/exceptions/ArgumentException.java
@@ -3,6 +3,6 @@ package smpl.exceptions;
 public class ArgumentException extends RuntimeException {
 
     public ArgumentException(int paramSize, int argSize) {
-        super(String.format("Expected %d arguments but got %d", paramSize, argSize));
+        super(String.format("Expected %d argument(s) but got %d", paramSize, argSize));
     }
 }
