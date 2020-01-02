8e8a7acbfe61ab9bf52eaa3a22f70657e128b3e6
Kayon-Marie <k.marie.douglas@hotmail.com>
2020-01-01 23:33:06 -0500

    adding classes and methods for built ins

---
+++ src/smpl/syntax/ast/ExpEq.java
@@ -0,0 +1,35 @@
+package smpl.syntax.ast;
+
+import smpl.exceptions.VisitException;
+import smpl.semantics.Visitor;
+import smpl.syntax.ast.core.Exp;
+import smpl.values.*;
+
+public class ExpEq extends Exp {
+
+    String exp1, exp2;
+
+    public ExpEq(String exp1, String exp2) {
+        this.exp1 = exp1;
+        this.exp2 = exp2;
+    }
+
+    public String getArg1(){
+        return this.exp1;
+    }
+
+    public String getArg2(){
+        return this.exp2;
+    }
+
+    @Override
+    public <S, T> T visit(Visitor<S, T> v, S arg) throws VisitException {
+        return v.visitExpEq(this, arg);
+    }
+
+    @Override
+    public String toString() {
+        return "eqv?("+this.exp1+","+this.exp2+")";
+    }
+
+}
---
+++ src/smpl/syntax/ast/ExpSize.java
@@ -0,0 +1,30 @@
+package smpl.syntax.ast;
+
+import smpl.exceptions.VisitException;
+import smpl.semantics.Visitor;
+import smpl.syntax.ast.core.Exp;
+import smpl.values.*;
+
+public class ExpSize extends Exp {
+
+    String exp1;
+
+    public ExpSize(String exp1) {
+        this.exp1 = exp1;
+    }
+
+    public String getArg(){
+        return this.exp1;
+    }
+
+    @Override
+    public <S, T> T visit(Visitor<S, T> v, S arg) throws VisitException {
+        return v.visitExpSize(this, arg);
+    }
+
+    @Override
+    public String toString() {
+        return "size("+this.exp+")";
+    }
+
+}
